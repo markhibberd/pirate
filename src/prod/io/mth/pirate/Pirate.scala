836ec0e83dccb61f56a00b17cd70112c307d459f
Mark Hibberd <mth@fire.markh.trinity>
2010-10-18 22:06:36 +1000

    initial

---
+++ src/prod/io/mth/pirate/Pirate.scala
@@ -0,0 +1,50 @@
+package io.mth.pirate
+
+import scalaz._
+
+case class Flag()
+
+trait Pirate[A] {
+  def fold[T](
+    flag: String => String => String => (A => A) => T,
+    line: String => List[Pirate[A]] => T
+  ): T
+
+  def usage: String = fold(
+     short => long => desc => f => short + ", " + long + "\t\t" + desc,
+     command => flags => command + " [OPTIONS]\n\t\t" + flags.map(_.usage).mkString("\n\t\t")
+  )
+
+  def parserise: PirateParser[A => A] = {
+    import PirateParser._
+    fold(
+      short => long => desc => f => flag(short, long).lift(_ => f),
+      command => flags => flatCommandline(flags.map(_.parserise), List())
+    )
+  }
+}
+
+
+object Pirate {
+  def flag[A](short: String, long: String, desc: String, f: A => A): Pirate[A] = new Pirate[A] {
+    def fold[T](
+      flag: String => String => String => (A => A) => T,
+      line: String => List[Pirate[A]] => T
+    ) = flag(short)(long)(desc)(f)
+  }
+
+  def line[A](command: String, flags: List[Pirate[A]]): Pirate[A] = new Pirate[A] {
+    def fold[T](
+      flag: String => String => String => (A => A)  => T,
+      line: String => List[Pirate[A]] => T
+    ) = line(command)(flags)
+  }
+
+  def parse[A](p: Pirate[A], args: List[String], a: A): Option[A] =
+    p.parserise.parse(args) match {
+      case Success((rest, f)) => Some(f(a))
+      case Failure(msg) => None
+    }
+}
+
+
---
+++ src/test/scala/io/mth/pirate/demo/Demo.scala
@@ -0,0 +1,23 @@
+package io.mth.pirate.demo
+
+import io.mth.pirate.Pirate
+
+object Demo {
+  import Pirate._
+  case class DemoArgs(help: Boolean, version: Boolean, verbose: Boolean, expression: Option[String], console: Boolean, config: Option[String])
+
+  val helparg = flag("-h", "--help", "display usage.", (d: DemoArgs) => d.copy(help = true))
+  val versionarg = flag("-V", "--version", "display version.", (d: DemoArgs) => d.copy(version = true))
+  val verbosearg = flag("-v", "--verbose", "verbose output.", (d: DemoArgs) => d.copy(verbose = true))
+  val demo = line("demo", List(helparg, versionarg, verbosearg))
+
+  def main(args: Array[String]) {
+    println(demo.usage)
+
+    val p = demo.parserise
+
+    // FIX fail on unexpected input...
+    val result = parse(demo, List("--version", "-h", "-x"), DemoArgs(false, false, false, None, false, None))
+    println(result)
+  }
+}
