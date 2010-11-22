c43d7fcd48222ff6fb36d20f366bf2bf54b777e5
Mark Hibberd <mark@hibberd.id.au>
2010-11-22 21:34:42 +1000

    combinator cleanup

--- src/prod/io/mth/pirate/Parser.scala
+++ src/prod/io/mth/pirate/Parser.scala
@@ -2,30 +2,33 @@ package io.mth.pirate
 
 import scalaz._
 import Scalaz._
+import Monad._
 
+// FIX This is a simple (poor mans) combinator library - not very generic.
+// FIX Wire in scalaz.Monad and kill the dupe.
 trait Parser[+A] {
   def parse(args: List[String]): Validation[String, (List[String], A)]
 
-  // FIX work out why I can't point free these without type annotating up the wazoo.....
-  def lift[B](f: A => B) : Parser[B] = Parser.lift(this)(f)
+  def lift[B](f: A => B) = Parser.lift(this)(f)
 
-  def lift2[B, C](p: Parser[B])(f: A => B => C) = Parser.lift2(this)(p)(f)
+  def lift2[B, C](p: => Parser[B])(f: (A, B) => C) = Parser.lift2(this)(p)(f)
+
+  def >>=[B](f: A => Parser[B]) = Parser.bind(this, f)
+
+  def >>>[B](f: => Parser[B]) = Parser.bind(this, (_:A) => f)
+
+  def |[B >: A](p: => Parser[B]) = Parser.choice(this, p)
+
+  def * = Parser.list(this)
+
+  def + = Parser.many1(this)
+
+  def rep(n: Int) = Parser.thisMany(n, this)
 }
 
 object Parser {
   def value[A](v: A) = new Parser[A] {
     def parse(args: List[String]) = Success((args, v))
-  }
-
-  def failed[A](msg: String) = new Parser[A] {
-    def parse(args: List[String]) = Failure(msg)
-  }
-
-  def string = new Parser[String] {
-    def parse(args: List[String]) = args match {
-      case Nil => Failure("Unexpected end of input.")
-      case head :: tail => Success((tail, head))
-    }
   }
 
   def bind[A, B](p: Parser[A], f: A => Parser[B]) = new Parser[B] {
@@ -35,60 +38,76 @@ object Parser {
     }
   }
 
-  def oneOf[A](p1: Parser[A], p2: Parser[A]) = new Parser[A] {
+  def failed[A](msg: String): Parser[A] = new Parser[A] {
+    def parse(args: List[String]) = Failure(msg)
+  }
+
+  def zerox[A] = failed[A]("zero")
+
+  def string = new Parser[String] {
+    def parse(args: List[String]) = args match {
+      case Nil => Failure("Unexpected end of input.")
+      case head :: tail => Success((tail, head))
+    }
+  }
+
+  def satisfyFor(pred: String => Boolean)(s: String) = 
+    if (pred(s))
+      value(s)
+    else
+      failed("Unexpected string [" + s + "]")
+
+  def satisfy(pred: String => Boolean) =
+    string >>= satisfyFor(pred)
+
+  def is(s: String) =
+    satisfy(_ == s)
+  
+  def choice[A](p1: Parser[A], p2: Parser[A]) = new Parser[A] {
     def parse(args: List[String]) = p1.parse(args) match {
       case Success((rest, value)) => Success((rest, value))
       case Failure(error) => p2.parse(args)
     }
   }
 
-  // FIX should be using fold... ?
-  def anyOneOf[A](ps: List[Parser[A]]): Parser[A] = ps.reduceRight((p: Parser[A], acc: Parser[A]) => oneOf(acc, p))
+  def choiceN[A](ps: List[Parser[A]]): Parser[A] =
+    ps.foldRight(zerox[A])(_ | _)
 
   def sequence[A](ps: List[Parser[A]]): Parser[List[A]] =
-    if (ps.empty) value(List[A]())
-    else lift2(ps.head)(sequence(ps.tail))(x => xs => x :: xs)
+    if (ps.isEmpty)
+      value(List[A]())
+    else
+      ps.head.lift2(sequence(ps.tail))(_::_)
 
-  def satisfy(pred: String => Boolean) = bind(string, (s: String) => if (pred(s)) value(s) else failed("Unexpected string [" + s + "]"))
+  def thisMany[A](n: Int, p1: Parser[A]) =
+    sequence((for (i <- 1 to n) yield p1).toList)
 
-  def is(s: String) = satisfy(_ == s)
+  def list[A](p: Parser[A]): Parser[List[A]] =
+      many1(p) | value(List[A]())
 
-  def list[A](p: Parser[A]): Parser[List[A]] = oneOf(many(p), value(List[A]()))
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    p.lift2(list(p))(_::_)
 
-  def many[A](p: Parser[A]): Parser[List[A]] = lift2(p)(list(p))(x => xs => x :: xs)
+  def lift[A, B](p: => Parser[A])(f: A => B) =
+    p >>= { (a: A) => value(f(a)) }
 
-  def lift[A, B](p: Parser[A])(f: A => B) = bind(p, (a: A) => value(f(a)))
+  def lift2[A, B, C](pa: => Parser[A])(pb: => Parser[B])(f: (A, B) => C) =
+    pa >>= { (a: A) => pb.lift(f(a, _)) } 
 
-  def lift2[A, B, C](pa: Parser[A])(pb: Parser[B])(f: A => B => C) =  bind(pa, (a: A) => lift(pb)(f(a)))
-
-  //----------------------------------------------------------------------------------------------
-
-
-
-  def flag(short: String, long: String) =  oneOf(is(short), is(long))
-
-  def endargs = is("--")
-
-  // FIX can this be defined in terms of many/list
-  def flagParser[A](p: Parser[A]): Parser[List[A]] = new Parser[List[A]] {
-    def parse(args: List[String]) = endargs.parse(args) match {
-      case Success((rest, value)) => Failure("End of flags reached")
-      case Failure(error) => oneOf(flagParserx(p), value(List[A]())).parse(args)
-    }
+  // FIX Complete and use this properly to remove some of the dupe ^^^^
+  implicit def ParserPure: Pure[Parser] = new Pure[Parser] {
+    def pure[A](a: => A) = value(a)
   }
 
-  def flagParserx[A](p: Parser[A]): Parser[List[A]] = lift2(p)(flagParser(p))(x => xs => x :: xs)
-
-  def commandline[A](flags: List[Parser[A => A]], positional: List[Parser[A => A]]) = new Parser[List[A => A]] {
-    def parse(args: List[String]) = flagParser(anyOneOf(flags)).parse(args) match {
-      case Success((rest, value)) => sequence(positional).parse(rest) match {
-         case Success((rest2, value2)) => Success(rest2, value ::: value2)
-         case Failure(error) => Failure(error)
-      }
-      case Failure(error) => sequence(positional).parse(args)
-    }
+  implicit def ParserBind: Bind[Parser] = new Bind[Parser] {
+    def bind[A, B](p: Parser[A], f: A => Parser[B]) = p >>= f
   }
 
-  def flatCommandline[A](flags: List[Parser[A => A]], positional: List[Parser[A => A]]) =
-    lift(commandline(flags, positional))(_.foldRight((a: A) => a)(_.compose(_)))
+  implicit def ParserMonad[A] = monad[Parser](ParserBind, ParserPure)
+
+  implicit def ParserZero[A]: Zero[Parser[A]] = zero(zerox)
+
+  implicit def ParserFunctor: Functor[Parser] = new Functor[Parser] {
+    def fmap[A, B](a: Parser[A], f: A => B) = a.lift(f)
+  }
 }
---
+++ src/test/scala/io/mth/pirate/test/ParserManualTest.scala
@@ -0,0 +1,84 @@
+package scala.io.mth.pirate.test
+
+import org.scalatest.FunSuite
+import io.mth.pirate.{Flag, Parser}
+
+// FIX Replace with an automated scalacheck test.
+class ParserManualTest extends FunSuite {
+  import Flag._
+  import Data.CannedFlags._
+
+  test("simple flags") {
+    check(f,  List("-a", "-b"),           List("-b"),            Map(("a", "")))
+    check(f,  List("--aa", "--bb", "-c"), List("--bb", "-c"),    Map(("a", "")))
+    check(f1, List("-b", "x", "-a"),      List("-a"),            Map(("b", "x")))
+    check(f1, List("--bb", "x", "--aa"),  List("--aa"),          Map(("b", "x")))
+
+    check(s,  List("-c", "-b"),           List("-b"),            Map(("c", "")))
+    check(s1, List("-d", "x", "-a"),      List("-a"),            Map(("d", "x")))
+
+    check(l,  List("--ee", "--bb", "-c"), List("--bb", "-c"),    Map(("e", "")))
+    check(l1, List("--ff", "x", "--aa"),  List("--aa"),          Map(("f", "x")))
+
+    bomb(s, List("--cc"))
+    bomb(f, List("a", "b", "c"))
+  }
+
+  test("combined flags") {
+    check(c1, List("-a", "-c", "--ee"),       List(),                Map(("a", ""), ("c", ""), ("e", "")))
+    check(c1, List("-a", "-c", "-d", "--ee"), List("-d", "--ee"),    Map(("a", ""), ("c", "")))
+    check(c1, List("--aa", "-c", "--ee"),     List(),                Map(("a", ""), ("c", ""), ("e", "")))
+
+    check(c2, List("-b", "x", "-d", "y", "--ff", "z"),        List(),                 Map(("b", "x"), ("d", "y"), ("f", "z")))
+    check(c2, List("-b", "x", "-d", "y", "-a", "--ff", "z"),  List("-a", "--ff", "z"), Map(("b", "x"), ("d", "y")))
+    check(c2, List("--bb", "x", "-d", "y", "--ff", "z"),      List(),                 Map(("b", "x"), ("d", "y"), ("f", "z")))
+
+    check(c3, List("-a", "-b", "x"),     List(), Map(("a", ""), ("b", "x")))
+    check(c3, List("--aa", "--bb", "x"), List(), Map(("a", ""), ("b", "x")))
+    check(c3, List("-a", "--bb", "x"),   List(), Map(("a", ""), ("b", "x")))
+    check(c3, List("--aa", "-b", "x"),   List(), Map(("a", ""), ("b", "x")))
+    check(c3, List("-b", "x", "-a"),     List(), Map(("a", ""), ("b", "x")))
+    check(c3, List("-b", "x", "--aa"),   List(), Map(("a", ""), ("b", "x")))
+    check(c3, List("--bb", "x", "-a"),   List(), Map(("a", ""), ("b", "x")))
+    check(c3, List("--bb", "x", "--aa"), List(), Map(("a", ""), ("b", "x")))
+
+    check(c6, List("-a", "-b", "x", "-c", "-d", "y", "--ee", "--ff", "z"), List(), Map(("a", ""), ("c", ""), ("e", ""), ("b", "x"), ("d", "y"), ("f", "z")))
+    check(c6, List("-b", "x", "-a", "-c", "-d", "y", "--ee", "--ff", "z"), List(), Map(("a", ""), ("c", ""), ("e", ""), ("b", "x"), ("d", "y"), ("f", "z")))
+    check(c6, List("--ee", "--ff", "z", "-b", "x", "-a", "-c", "-d", "y"), List(), Map(("a", ""), ("c", ""), ("e", ""), ("b", "x"), ("d", "y"), ("f", "z")))
+    check(c6, List("--ff", "z", "-b", "x", "--ee", "-a", "-c", "-d", "y"), List(), Map(("a", ""), ("c", ""), ("e", ""), ("b", "x"), ("d", "y"), ("f", "z")))
+  }
+
+  test("break args") {
+    check(c6, List("-a", "-b", "x", "--", "-c", "-d", "y", "--ee", "--ff", "z"), List("-c", "-d", "y", "--ee", "--ff", "z"), Map(("a", ""), ("b", "x")))
+  }
+
+  test("simple positional args") {
+    val one = c6 >>= positional1("FRED")((s: String) => (m: Map[String, String]) => m + (("FRED", s)))
+    val two = one >>= positionalN("BARNEY")((ss: List[String]) => (m: Map[String, String]) => m + (("BARNEY", ss.mkString(","))))
+
+    check(one, List("fred"),                         List(), Map(("FRED", "fred")))
+
+    check(two, List("fred", "barney"),               List(), Map(("FRED", "fred"), ("BARNEY", "barney")))
+    check(two, List("fred"),                         List(), Map(("FRED", "fred"), ("BARNEY", "")))
+    check(two, List("fred", "b1", "b2"),             List(), Map(("FRED", "fred"), ("BARNEY", "b1,b2")))
+    check(two, List("-a", "--", "-c", "--ff", "z"),  List(), Map(("FRED", "-c"), ("BARNEY", "--ff,z"), ("a", "")))
+  }
+
+  def bomb(f: Flag[Map[String, String]], args: List[String]) =
+    assert(p(f, args).fold(_ => true, _ => false))
+
+  def check(f: Flag[Map[String, String]], args: List[String], rest: List[String], expected: Map[String, String]) = {
+    val result = p(f, args)
+    assert(result.fold(
+        _ => false,
+        { case (r, f) => r == rest && f(Map()) == expected}
+      ), result.fold(
+        msg => "failed to parse [" + msg + "]",
+        { case (r, f) => "rest[" + r + "], expected[" + rest + "], [" + (r == rest) + "]\nvalue[" + f(Map()) + "], expected[" + expected + "], [" + (f(Map()) == expected) + "]\n\n" }
+      ))
+  }
+
+  def p(f: Flag[Map[String, String]], args: List[String]) =
+      f.toParser.parse(args.toList)
+
+}
