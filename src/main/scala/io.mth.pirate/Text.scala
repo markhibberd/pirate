33199218de9c83026f4f626ce76b032df6828db6
Charles O'Farrell <charleso@charleso.org>
2014-07-13 14:10:55 +1000

    Optimise the shit out of Text.wrap() for no good reason

--- src/main/scala/io.mth.pirate/Text.scala
+++ src/main/scala/io.mth.pirate/Text.scala
@@ -12,16 +12,28 @@ object Text {
   def wrap(text: String, width: Int, indent: Int): String = {
     val spacer = space(indent)
 
-    def wrapit(s: String, w: Int): String =
-      if (s.length <= w)
-        s
-      else if (w <= 0)
-        s.substring(0, width - 1) + "-\n" + spacer + wrapit(s.substring(width - 1), width)
-      else if (s.charAt(w) == ' ')
-        s.substring(0, w) + "\n" + spacer + wrapit(s.substring(w + 1), width)
-      else
-        wrapit(s, w - 1)
+    // Add 1 for hyphen + newline
+    val sb = new StringBuilder(text.length + (text.length / width * (spacer.length + 2)))
+    val s = spacer + text
 
-    wrapit(spacer + text, width)
+    @annotation.tailrec
+    def wrapit(o: Int, w: Int): Unit = {
+      val i = o + w
+      if (s.length <= i)
+        sb ++= s.substring(o)
+      else if (w <= 0) {
+        sb ++= s.substring(o, o + width - 1) ++= "-\n" ++= spacer
+        wrapit(o + width - 1, width)
+      } else if (s.charAt(i) == ' ') {
+        sb ++= s.substring(o, i) ++= "\n" ++= spacer
+        wrapit(i + 1, width)
+      }
+      else
+        wrapit(o, w - 1)
+    }
+
+    // Use a bullshit offset because String.substring() copies a new array on Java 1.7+
+    wrapit(0, width)
+    sb.toString()
   }
 }
