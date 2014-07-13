432e4163ba2e7c8a588709d04a4a687dc809bfcf
Charles O'Farrell <charleso@charleso.org>
2014-07-13 20:05:36 +1000

    Declare type for Interpretter.run

--- src/main/scala/io.mth.pirate/Interpretter.scala
+++ src/main/scala/io.mth.pirate/Interpretter.scala
@@ -5,6 +5,6 @@ import scalaz._, Scalaz._, \&/._
 object Interpretter extends Interpretter
 
 trait Interpretter {
-  def run[A](p: Parse[A], args: List[String]) =
+  def run[A](p: Parse[A], args: List[String]): PError \/ A =
     ParseTools.runParserFully(SkipOpts, p, args).run(NullPPrefs)
 }
