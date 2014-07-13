3f79ac6251c6e0570f99fd56fd66ae686d58ffa5
markhibberd <mark@hibberd.id.au>
2014-07-14 01:34:49 +1000

    More cleanup, examples and tests.

--- src/test/scala/io.mth.pirate.example/GitExample.scala
+++ src/test/scala/io.mth.pirate.example/GitExample.scala
@@ -17,7 +17,7 @@ object GitInfoPath extends GitCommand
 case class GitHelp(command: Option[String]) extends GitCommand
 
 
-object GitExample extends PirateMainIO[Git] {
+object GitMain extends PirateMainIO[Git] {
   val version: Parse[GitCommand] =
     terminator("version", GitVersion)
 
@@ -50,7 +50,8 @@ object GitExample extends PirateMainIO[Git] {
      git { help } |||
      git { html } |||
      git { man } |||
-     git { info }) ~ "git"
+     git { info }) ~ "git" ~~
+      "This is a demo of the git command line"
 
   def run(a: Git) = a.cmd match {
     case GitVersion => IO.putStrLn("git the pirate version")
@@ -62,4 +63,43 @@ object GitExample extends PirateMainIO[Git] {
       case Some(c) => sys.error("Implement sub-command usage printing.")
     }
   }
+}
+
+class GitExample extends test.Spec { def is = s2"""
+
+  Git Examples
+  ============
+
+  git --version                            $version
+  git --help                               $help
+  git --help status                        $helpAt
+
+  Git Checks
+  ==========
+
+  Name is set                              ${GitMain.command.name == "git"}
+  Description is set                       ${GitMain.command.description == Some("This is a demo of the git command line")}
+
+"""
+
+  def run(args: String*): ParseError \/ Git =
+    Interpretter.run(GitMain.command.parse, args.toList)
+
+  // FIX make these work
+
+  def version = {
+    run("--version") must_==
+      GitVersion
+  }.pendingUntilFixed
+
+  def help = {
+    run("--help") must_==
+      GitHelp(None)
+  }.pendingUntilFixed
+
+  def helpAt = {
+    run("--help", "status") must_==
+      GitHelp(Some("status"))
+  }.pendingUntilFixed
+
 }
