7fb0ddeeabcc10c56301083dc090a7be0bbbb595
markhibberd <mark@hibberd.id.au>
2015-02-05 08:41:16 +1100

    FIx version.

--- project/build.scala
+++ project/build.scala
@@ -11,7 +11,7 @@ object build extends Build {
   , settings = Defaults.coreDefaultSettings ++ Seq[Sett](
       name := "pirate"
     , organization := "io.mth"
-    , version in ThisBuild := "1.0-M1"
+    , version in ThisBuild := "1.0"
     , scalaVersion := "2.11.2"
     , crossScalaVersions := Seq("2.10.4", scalaVersion.value)
     , scalacOptions := Seq(
