97280498521536f73b0ae9ff30549221d23e654e
sanjiv sahayam <sanjsmailbox@gmail.com>
2015-05-30 17:11:45 +1000

    Added property test for name and description set.

--- src/test/scala/pirate/ParseSpec.scala
+++ src/test/scala/pirate/ParseSpec.scala
@@ -5,9 +5,10 @@ class ParseSpec extends spec.Spec { def is = s2"""
   Parse Properties
   ================
 
-  Verify basic name pass-through to command       $name
-  Verify description not set on command           $desc
-  Verify description can be set on command        $descSet
+  Verify basic name pass-through to command                $name
+  Verify description not set on command                    $desc
+  Verify description can be set on command                 $descSet
+  Verify name and description can be set on command        $nameAndDescSet
 
 """
   def name = prop((n: String) =>
@@ -19,4 +20,8 @@ class ParseSpec extends spec.Spec { def is = s2"""
   def descSet = prop((n: (String, String)) =>
     (ValueParse(None) ~ n._1 ~~ n._2).description === Some(n._2))
 
+  def nameAndDescSet = prop((n: (String, String)) => {
+   val parse = ValueParse(None) ~ n._1 ~~ n._2
+   parse.name === n._1 && parse.description === Some(n._2)
+  })
 }
